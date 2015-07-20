bedd3f1de2ef50541703d20524a12d4edb0c4e9c
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-07-20 11:47:46 +0200

    Strip down API

--- click_log/__init__.py
+++ click_log/__init__.py
@@ -4,7 +4,7 @@ import click
 import logging
 import functools
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 LOGGER_KEY = __name__ + '.logger'
 
 
@@ -42,27 +42,9 @@ _default_handler = logging.StreamHandler(ClickStream())
 _default_handler.formatter = ColorFormatter()
 
 
-def _default_click_config(logger):
+def basic_config(logger=None):
+    if not isinstance(logger, logging.Logger):
+        logger = logging.getLogger(logger)
     logger.handlers = [_default_handler]
     logger.setLevel(logging.INFO)
-
-
-def get_logger(ensure=True):
-    meta = click.get_current_context().meta
-    logger = meta.get(LOGGER_KEY)
-    if logger is None and ensure:
-        meta[LOGGER_KEY] = logger = logging.getLogger()
-        _default_click_config(logger)
     return logger
-
-
-def set_logger(logger):
-    click.get_current_context().meta[LOGGER_KEY] = logger
-
-
-def pass_logger(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        return f(get_logger(), *args, **kwargs)
-
-    return wrapper
