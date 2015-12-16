ed2bea0b4a25cf94d5075eeb75dcdba1d91ee39d
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-12-16 19:01:50 +0100

    Add one simple test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,8 +20,9 @@ def test_basic(runner):
     @click.command()
     @click_log.init()
     def cli():
+        test_logger.info('hey')
         test_logger.error('damn')
 
     result = runner.invoke(cli, catch_exceptions=False)
     assert not result.exception
-    assert result.output == 'error: damn\n'
+    assert result.output == 'hey\nerror: damn\n'
