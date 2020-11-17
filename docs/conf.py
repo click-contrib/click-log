2340926e6c209eee9684e1fbe6ffd27ab283b8df
Julien <dev@ingeoveritas.com>
2020-11-17 11:26:28 +0100

    Remove useless html_theme_path

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,6 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 try:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 except ImportError:
     html_theme = 'default'
     if not on_rtd:
